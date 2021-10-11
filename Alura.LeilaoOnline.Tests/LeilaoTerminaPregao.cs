462fa21c8eaccb6bd5cfbb9653573bf171536449
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-10-11 16:01:04 -0300

    Organizando os testes

--- Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -1,26 +1,23 @@
 ﻿using Alura.LeilaoOnline.Core;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace Alura.LeilaoOnline.Tests
 {
-    public class LeilaoTestes
+    public class LeilaoTerminaPregao
     {
         [Theory]
         [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
         [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
         [InlineData(800, new double[] { 800 })]
-        public void LeilaoComVariosLances(double valorEsperado, double[] ofertas)
+        public void RetornaMaiorValorDadoLeilaoComPeloMenosUmLance(double valorEsperado, double[] ofertas)
         {
             // Arranjo - cenário
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
 
             // Act - método sob teste
-            leilao.TerminaPregao();
-
             foreach (var valor in ofertas)
             {
                 leilao.RecebeLance(fulano, valor);
@@ -33,7 +30,7 @@ namespace Alura.LeilaoOnline.Tests
         }
 
         [Fact]
-        public void LeilaoSemLanche()
+        public void RetornaZeroDadoLeilaoSemLances()
         {
             // Arranjo - cenário
             var leilao = new Leilao("Van Gogh");
