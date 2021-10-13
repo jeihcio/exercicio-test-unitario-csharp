a07df62350a702c7bc7da645050c010d93163514
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-10-13 10:04:14 -0300

    Alterando o nome do arquivo

--- Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
+++ Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace Alura.LeilaoOnline.Tests
 {
-    public class LeilaoRecebeLance
+    public class LeilaoRecebeOferta
     {
         [Theory]
         [InlineData(4, new double[] { 100, 1200, 1400, 1300 })]
